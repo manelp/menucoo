d5d34581ab188e3b15459dbdd18b9b4f4e9cd151
Manel Perez <manel@perezbondia.com>
2022-11-07 23:27:11 +0100

    test api method (not implemented)

---
+++ menucoo-api/src/test/scala/com/menucoo/api/WeekMenuApiTest.scala
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2022 Manel Perez
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package com.menucoo.api
+
+import munit.CatsEffectSuite
+import org.http4s.Status
+import cats.effect.kernel.Resource
+import org.http4s.HttpRoutes
+import org.http4s.circe._
+import org.http4s.implicits._
+import cats.effect.IO
+import cats.effect.kernel.Ref
+import org.http4s.server.Router
+import org.http4s.Uri
+
+import com.menucoo.test.TestHelpers._
+import org.http4s.Request
+import org.http4s.Method
+import org.typelevel.ci.CIString
+import com.menucoo.api.model.GenericError
+import org.http4s.EntityDecoder
+
+class WeekMenuApiTest extends CatsEffectSuite {
+
+  import JsonProtocol.given
+
+  given EntityDecoder[IO, GenericError]    = jsonOf
+  
+
+  test("GET /api/v1/menu/{id}/week returns week menu"){
+    val expectedStatusCode  = Status.BadRequest
+    
+    val expectedContentType = "application/json"
+    val expectedBody = GenericError("not.implemented", "Not implemented")
+
+    val response = testResources().use { service =>
+      for {
+        uri <- Uri.fromString("/api/v1/menu/1/week").toOption.getOrThrow
+        request = Request[IO](method = Method.GET, uri = uri)
+        response <- service.orNotFound.run(request)
+      } yield response
+    }
+
+    val test = for {
+      result      <- response
+      body        <- result.as[GenericError]
+      contentType <- result.headers.get(CIString("content-type")).getOrThrow
+    } yield (result.status, body, contentType.head.value)
+    test.assertEquals((expectedStatusCode, expectedBody, expectedContentType))
+
+  }
+
+  def testResources(): Resource[IO, HttpRoutes[IO]] =
+    Resource.eval(IO(Router("/" -> new WeekMenuApi[IO]().routes)))
+    
+}
